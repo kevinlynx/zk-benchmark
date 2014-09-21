bfd5d01a466c0ab1578d3c01212c8beee1a21ce2
qiyan <qiyan.zm@alibaba-inc.com>
2014-09-21 19:24:05 +0800

    add more watch client

--- result/watch.plt
+++ result/watch.plt
@@ -5,6 +5,9 @@ set xrange [0:110000]
 set xtics 10000, 10000, 100000
 set yrange [0:8000]
 set ytics 0, 1000, 8000
-plot "watch.dat" using 1:2 w lp pt 5 title "watch", "watch.dat" using 1:3 w lp pt 7 title "without watch"
+plot "watch.dat" using 1:2 w lp pt 5 title "0 watch", \
+    "watch.dat" using 1:3 w lp pt 7 title "1 watch", \
+    "watch.dat" using 1:4 w lp pt 7 title "3 watch", \
+    "watch.dat" using 1:5 w lp pt 7 title "5 watch"
 
 
--- zk-watch.py
+++ zk-watch.py
@@ -17,12 +17,12 @@ parser.add_option("", "--root_znode", dest="root_znode",
                   default="/zk-latencies", help="root for the test, default /zk-latencies")
 parser.add_option("", "--znode_count", dest="znode_count", default=10000, type="int",
                   help="the number of znodes to operate on in each performance section (default %default)")
-parser.add_option("", "--watch_multiple", dest="watch_multiple", default=1, type="int",
-                  help="number of watches to put on each znode (default %default)")
+parser.add_option("", "--watch_session", dest="watch_session", default=1, type="int",
+                  help="number of watch sessions to put on each znode (default %default)")
 parser.add_option("", "--znode_size", dest="znode_size", type="int",
                   default=25, help="data size when creating/setting znodes (default %default)")
 parser.add_option("", "--next_watch", dest="next_watch", action="store_true",
-                  default=False, help="data size when creating/setting znodes (default %default)")
+                  default=False, help="continue to watch (default %default)")
 parser.add_option("-v", "--verbose",
                   action="store_true", dest="verbose", default=False,
                   help="verbose output, include more detail")
@@ -147,21 +147,24 @@ if __name__ == '__main__':
     data = options.znode_size * "x"
     servers = get_zk_servers()
 
-    session_get = ZKClient(servers, options.timeout)
+    session_gets = []
+    for x in xrange(options.watch_session):
+        session_gets.append(ZKClient(servers, options.timeout))
     session_set = ZKClient(servers, options.timeout)
 
     session_set.create(options.root_znode,
         "smoketest root, delete after test done, created %s" % (datetime.datetime.now().ctime()))
 
     create_nodes(session_set)
-    for x in xrange(options.watch_multiple):
-        add_wathches(session_get)
+    for s in session_gets:
+        add_wathches(s)
     set_nodes(session_set, data)
     delete_nodes(session_set) 
 
     time.sleep(10)
     session_set.delete(options.root_znode)
-    session_get.close()
+    for s in session_gets:
+        s.close()
     session_set.close()
     print 'watch test complete'
 
